6a439e0842827848610b04238ef1ec17dd1d1f39
TaherJalal <taherjalaljuma@gmail.com>
2024-03-16 21:40:32 +0300

    missing with vue

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -4,20 +4,22 @@
 
 <template>
   <div class="hero">
-  <h1 v-motion-slide-visible-right>Hero section</h1>
+    <h1 v-motion-slide-visible-right>Hero section</h1>
   </div>
 </template>
 
 <style scoped>
-.hero{
-margin: 0;
+.hero {
+  position: relative;
+  margin: 0;
   height: 40vh;
   width: 100%;
   background-color: black;
   color: white;
 }
-.hero h1{
+
+.hero h1 {
   padding: 2vw;
-    font-size: xxx-large;
-  }
+  font-size: xxx-large;
+}
 </style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -4,28 +4,35 @@
 
 <template>
   <nav class="navbar">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/tc">T&C</RouterLink>
+    <div class="navbar">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/tc">T&C</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/register">Register</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+    </div>
   </nav>
 </template>
 
 <style scoped>
-  .navbar{
-    padding-top: 2vh;
-    padding-left: 8vw;
-    padding-bottom: 2vh;
-    margin: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    gap: 4vw;
-    font-weight: bold;
-    background-color: aquamarine;
-  }
+.navbar {
+  padding-top: 1vh;
+  padding-left: 8vw;
+  padding-bottom: 1vh;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 4vw;
+  font-weight: bold;
+  background-color: aquamarine;
+}
 
-  .navbar a {
-    text-decoration: none !important;
-    color: black;
-  }
+.navbar a {
+  text-decoration: none !important;
+  color: black;
+}
 </style>
